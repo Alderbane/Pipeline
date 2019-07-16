5a009dc01ef926d3e7a4f5f47421a0fca3d3bfd5
stompercito <51408323+stompercito@users.noreply.github.com>
2019-07-16 16:04:57 -0500

    Added hazard Unit Skeleton

---
+++ HazardUnit.v
@@ -0,0 +1,12 @@
+module HazardUnit
+(
+input [4:0] Instruction_ID,
+input [4:0] MemRead_EX,
+input [4:0] RT_EX,
+output [1:0] DWrite,
+output [1:0] PCWrite,
+output [1:0] Hazardflush
+
+);
+
+endmodule
