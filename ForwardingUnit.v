d5fbb4f35cb2660e54b7320fd955e4c5d3aea094
stompercito <51408323+stompercito@users.noreply.github.com>
2019-07-18 21:39:23 -0500

    Fixes were made

--- ForwardingUnit.v
+++ ForwardingUnit.v
@@ -16,7 +16,7 @@ assign A = 	(WB_MEM == 1'b1 &&  RTorRD_MEM != 0 && RS_EX == RTorRD_MEM)?								
 				(WB_WB == 1'b1 && RTorRD_WB != 0 && RTorRD_MEM != RS_EX && RS_EX == RTorRD_WB)?			2'b11:
 																																														2'b00;
 
-assign B = 	(RT_EX == RTorRD_MEM && WB_MEM == 1'b1 && RTorRD_MEM != 0)?											2'b10:
+assign B = 	(WB_MEM == 1'b1 && RTorRD_MEM != 0 && RT_EX == RTorRD_MEM)?	    								2'b10:
 				(RT_EX == RTorRD_WB && WB_WB == 1'b1 && RTorRD_MEM != RT_EX && RTorRD_WB != 0)?			2'b11:
 																																														2'b00;
 
